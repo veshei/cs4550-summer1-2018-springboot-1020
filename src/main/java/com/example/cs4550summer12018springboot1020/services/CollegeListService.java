360a51c0f0f2f24beca5626d6c60c1fc5f2de730
Veronica Shei <shei.v@husky.neu.edu>
2018-06-12 19:35:05 -0400

    added service class for user, college, recommendations, reviews, college lists, and reviews

--- src/main/java/com/example/cs4550summer12018springboot1020/services/CollegeListService.java
+++ src/main/java/com/example/cs4550summer12018springboot1020/services/CollegeListService.java
@@ -1,4 +1,12 @@
 package com.example.cs4550summer12018springboot1020.services;
 
+import com.example.cs4550summer12018springboot1020.repositories.CollegeListRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CollegeListService {
+  @Autowired
+  CollegeListRepository collegeListRepository;
 }
