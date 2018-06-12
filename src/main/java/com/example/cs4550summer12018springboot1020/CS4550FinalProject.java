6ebcebefb45e1c33b061201dc8d0516314292ebf
Veronica Shei <shei.v@husky.neu.edu>
2018-06-12 16:33:17 -0400

    added springboot project

---
+++ src/main/java/com/example/cs4550summer12018springboot1020/CS4550FinalProject.java
@@ -0,0 +1,12 @@
+package com.example.cs4550summer12018springboot1020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CS4550FinalProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CS4550FinalProject.class, args);
+	}
+}
---
+++ src/test/java/com/example/cs4550summer12018springboot1020/CS4550FinalProjectTests.java
@@ -0,0 +1,16 @@
+package com.example.cs4550summer12018springboot1020;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CS4550FinalProjectTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
