08915a13521a188112566b8c20b0287bb2edb6ad
Justin <jstnlee365@gmail.com>
2018-06-13 22:37:09 -0400

    Added spring configuration file for cross origin stuff

---
+++ src/main/java/com/example/cs4550summer12018springboot1020/MyConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.cs4550summer12018springboot1020;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowCredentials(true).maxAge(3600)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type")
+                .allowedOrigins("http://localhost:3000", "https://cs4550-summer1-2018-react-1020.herokuapp.com");
+            }
+        };
+    }
+}
