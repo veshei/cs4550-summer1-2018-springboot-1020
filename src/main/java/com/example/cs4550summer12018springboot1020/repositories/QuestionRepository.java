fcf010be961ede05543ceb3010fd606ec55de53e
Justin <jstnlee365@gmail.com>
2018-06-14 17:30:00 -0400

    Added query for finding questions for a college

--- src/main/java/com/example/cs4550summer12018springboot1020/repositories/QuestionRepository.java
+++ src/main/java/com/example/cs4550summer12018springboot1020/repositories/QuestionRepository.java
@@ -2,7 +2,19 @@ package com.example.cs4550summer12018springboot1020.repositories;
 
 import com.example.cs4550summer12018springboot1020.models.Question;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface QuestionRepository extends CrudRepository<Question, Integer> {
+
+  /**
+   * Returns all questions for the given college id.
+   * @param collegeId the id of the college
+   * @return the list of all questions for the given college
+   */
+  @Query("SELECT q FROM Question q WHERE q.collegeId=:collegeId")
+  List<Question> findQuestionsForCollege(@Param("collegeId") int collegeId);
 }
