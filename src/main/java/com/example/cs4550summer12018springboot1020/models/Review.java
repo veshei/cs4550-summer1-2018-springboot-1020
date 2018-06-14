e6441a99d06a7ef54a2a5b2626d9ea3cabf57c9e
Justin <jstnlee365@gmail.com>
2018-06-14 11:27:52 -0400

    Updated review backend functionalities to make reviews work with frontend

--- src/main/java/com/example/cs4550summer12018springboot1020/models/Review.java
+++ src/main/java/com/example/cs4550summer12018springboot1020/models/Review.java
@@ -14,6 +14,7 @@ public class Review {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String title;
+  private String body;
   private Integer collegeId;
   @ManyToOne
   @JsonIgnore
@@ -33,6 +34,14 @@ public class Review {
 
   public void setTitle(String title) {
     this.title = title;
+  }
+
+  public String getBody() {
+    return body;
+  }
+
+  public void setBody(String body) {
+    this.body = body;
   }
 
   public Integer getCollegeId() {
--- src/main/java/com/example/cs4550summer12018springboot1020/services/ReviewService.java
+++ src/main/java/com/example/cs4550summer12018springboot1020/services/ReviewService.java
@@ -1,14 +1,43 @@
 package com.example.cs4550summer12018springboot1020.services;
 
+import com.example.cs4550summer12018springboot1020.models.Review;
+import com.example.cs4550summer12018springboot1020.models.User;
 import com.example.cs4550summer12018springboot1020.repositories.ReviewRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ReviewService {
   @Autowired
   ReviewRepository reviewRepository;
+
+  /**
+   * Finds all reviews for the college of the given id.
+   * @param collegeId the id of the college
+   * @return a list of college reviews associated with the college id
+   */
+  @GetMapping("/api/review/college/{collegeId}")
+  public List<Review> findReviewsForCollege(@PathVariable("collegeId") int collegeId) {
+    return this.reviewRepository.findReviewsForCollege(collegeId);
+  }
+
+  /**
+   * Creates a new review with the creator of the review being the currently logged in user.
+   * @param review the new review
+   * @return the review created on success, null on failure
+   */
+  @PostMapping("/api/review")
+  public Review createReview(HttpSession session, @RequestBody Review review) {
+    User currentUser = (User) session.getAttribute("currentUser");
+    if (currentUser != null) {
+      review.setUser(currentUser);
+      this.reviewRepository.save(review);
+    }
+    return null;
+  }
 }
